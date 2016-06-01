66f22a36b50f737df7a54b12bffb78568de6608d
justincarothers <justincarothers@gmail.com>
2016-06-01 14:38:26 -0700

    added color picker, working on random background color jQuery animate function

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -1,19 +1,58 @@
-var add = function(num1, num2) {
+// ========== messing around with js plugins
+
+// var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) +
+var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256))
+
+
+// ======== BACK END ===========
+
+var addTheNumbers = function(num1, num2) {
   return num1 + num2
 };
 
+
+
+// ========= FRONT END ==================
+
+
 $(document).ready(function() {
+
+ spectrum();
+
+ function spectrum(){
+    var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    $('.colorbox').animate( { backgroundColor: hue }, 1000);
+    spectrum();
+ }
+
+});
+
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+
+  var favoriteColor = $("#color").val();
 
   $("button").click(function(event) {
 
-    event.preventDefault();
+
 
     var num1=parseInt($("#firstNumber").val());
     var num2=parseInt($("#secondNumber").val());
 
-    var result = add(num1, num2);
+    var result = addTheNumbers(num1, num2);
     alert(result);
+    event.preventDefault();
 
   })
+
+
+
 
 });
